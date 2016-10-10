ef82c5a934edc725e54de9870ebeff80d584df5a
micko45 <michaelmcgearty@gmail.com>
2016-10-10 23:19:04 +0100

    update

--- ham_radio/ham_lookup.rb
+++ ham_radio/ham_lookup.rb
@@ -1,33 +1,48 @@
 #!/usr/bin/env ruby
 
 module CwLookup
-#Countries lookup 
+#Countries lookup table
   countries  = {'ei' => 'Ireland','k' => 'USA', 'd' => 'Germany' }
 
   def CwLookup._splitCallSign(callSign)
-    String vari = ""
+    ctry = Hash[ 'code' => "",  'num' => "", 'id' => "" ]
+    lockcode, locknum  = 0
     callSign.split("").each do |i|
-      if i !~ /[0-9]/
-        vari = "#{vari}" + i
+      if i !~ /[0-9]/ and  lockcode == 0
+        ctry['code'] = ("#{ctry['code']}" + i).to_s
+      elsif i =~ /[0-9]/ || locknum == 0
+        ctry['num'] = "#{ctry['num']}" + i
+        #so we got the country code so we lock it out
+        #we need to now split out hte number. 
+        lockcode = 1
       else 
-        return vari.to_s
+         #So we got the num and we lock it out. 
+         ctry['id'] = "#{ctry['id']}" + i
+         locknum = 1
       end
     end
-    return vari.to_s
+        return ctry
   end
 
   def CwLookup._getCountryID(countryCode, countries)
+    array = Array.new
     if countries.has_key?(countryCode)
-      return "#{countryCode}, #{countries[countryCode]}"
+      array.push(countryCode, countries[countryCode])
+      return array
     else 
-      return "not found it, #{countryCode}"
+      return #"not found it, #{countryCode}"
     end
     return
   end
     
   print "call sign to lookup> "
-  choice = gets.chomp
-  #choice = $stdin.gets.chomp
-  puts CwLookup._getCountryID(CwLookup._splitCallSign(choice),countries)
+  choice = $stdin.gets.chomp
+
+  callSignHash = Hash[CwLookup._splitCallSign(choice)]
+  data = CwLookup._getCountryID(callSignHash['code'], countries)
+  data.push callSignHash['num']
+  data.push(callSignHash['id'])
+  print data
+puts
 
 end
