4e9289216a173c8b911d5dc100f4cf2c0776edc3
micko45 <michaelmcgearty@gmail.com>
2017-01-01 10:12:54 +0000

    update

--- ham_radio/api/access_qrz_api.rb
+++ ham_radio/api/access_qrz_api.rb
@@ -1,9 +1,12 @@
 #!/bin/env ruby
 #Use qrz_api to connect to qz and get some info. 
-
 require "./qrz_api.rb"
 require 'optparse'
 require './adif_api.rb'
+
+##############
+#SetUP
+##############
 
 #Create hash required by qrz api
 args = Hash[
@@ -12,30 +15,10 @@ args = Hash[
   :user => "ei5hsb",
   :pass => "Letmein_123" ]
 
-
-def getHistory(file, tosearch)
-  data = ADIF2Hash.hash(file)
-  testarray = ADIF2Hash.searchArray('call', tosearch, data)
-  return testarray.length
-end
-
-#sort and print output. 
-def printout(query,history)
-  puts "Callsign: #{query['Callsign']['call']}"
-  puts "Name: #{query['Callsign']['fname']} #{query['Callsign']['name']}"
-  puts "Address: #{query['Callsign']['addr2']}"
-  puts "Country: #{query['Callsign']['country']}"
-  puts "Previous Contacts: #{history}"
-  
-#  puts "Comment: Thks  #{query['Callsign']['fname']} for QSO, 73"
-  puts "https://www.qrz.com/db/#{query['Callsign']['call']}"
-end
-
 #Get the options
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: example.rb [options]"
-
   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
     options[:verbose] = v
   end
@@ -44,15 +27,41 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-###Main#####
-history = getHistory('../test.adi', options[:callsign])
+##################
+#functions
+##################
+
+def getCallHistory(file, tosearch)
+  data = ADIF2Hash.hash(file)
+  testarray = ADIF2Hash.searchArray('call', tosearch, data)
+  return testarray.length
+end
+
+#sort and print output. 
+def printout(qrzInfo,history)
+  puts "Callsign: #{qrzInfo['Callsign']['call']}"
+  puts "Name: #{qrzInfo['Callsign']['fname']} #{qrzInfo['Callsign']['name']}"
+  puts "Address: #{qrzInfo['Callsign']['addr2']}"
+  puts "Country: #{qrzInfo['Callsign']['country']}"
+  puts "Previous Contacts: #{history[:call]}"
+  puts "https://www.qrz.com/db/#{qrzInfo['Callsign']['call']}"
+end
+
+###########'
+#Main    #
+###########'
+qrzInfo = GetQRZ.callSignLookup(GetQRZ.login(args), options[:callsign], args)
+historyFile = '/mediashare/medishare/current_Log.ADI'
+history = Hash.new
+history[:call] = getCallHistory(historyFile, options[:callsign])
+
 if options[:callsign].to_s != ''
-    printout(GetQRZ.callSignLookup(GetQRZ.login(args), options[:callsign], args),history)
+    printout(qrzInfo, history)
 else
   print "call sign to lookup> "
   callsign = $stdin.gets.chomp
   if callsign == ""
     callsign = "ei5hsb"
   end
-  printout(GetQRZ.callSignLookup(GetQRZ.login(args), callsign, args),history)
+  printout(qrzInfo, history)
 end
